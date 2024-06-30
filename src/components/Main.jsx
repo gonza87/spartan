30739ed4ccfc8d89275361473940115005c1be5d
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-06-30 12:10:37 -0300

    animacionServices

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,66 @@
 import React from "react";
-
-import { GiCenturionHelmet } from "react-icons/gi";
-import { GiSwordWound } from "react-icons/gi";
-import { GiSwordwoman } from "react-icons/gi";
-import { GiDoorway } from "react-icons/gi";
+import { useInView } from "react-intersection-observer";
+import {
+  GiCenturionHelmet,
+  GiSwordWound,
+  GiSwordwoman,
+  GiDoorway,
+} from "react-icons/gi";
+import "animate.css";
 import TestimonialsCarousel from "./TestimonialsCarousel";
+
 function Main() {
+  const { ref: card1Ref, inView: card1InView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: card2Ref, inView: card2InView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: card3Ref, inView: card3InView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: card4Ref, inView: card4InView } = useInView({
+    triggerOnce: true,
+  });
+
   return (
     <div className="main">
-      {/* <img src=".././img/spartan04.jpg" alt="" className="main-background" /> */}
-      {/* <div className="combined-gradient"></div> */}
       <div className="main-content container">
         <h2 className="titleServices">Nuestros Servicios</h2>
         <div className="contServices">
-          <div className="text-center cardService">
+          <div
+            ref={card1Ref}
+            className={`text-center cardService ${
+              card1InView ? "animate__animated animate__fadeInUp" : ""
+            }`}
+          >
             <GiSwordwoman color="#D72323" size="6em" />
             <p>Seguridad y vigilancia</p>
           </div>
-          <div className="text-center cardService">
+          <div
+            ref={card2Ref}
+            className={`text-center cardService ${
+              card2InView ? "animate__animated animate__fadeInUp" : ""
+            }`}
+          >
             <GiDoorway color="#D72323" size="6em" />
             <p>Portería</p>
           </div>
-          <div className="text-center cardService">
+          <div
+            ref={card3Ref}
+            className={`text-center cardService ${
+              card3InView ? "animate__animated animate__fadeInUp" : ""
+            }`}
+          >
             <GiSwordWound color="#D72323" size="6em" />
             <p>Limpieza y mantenimiento</p>
           </div>
-          <div className="text-center cardService">
+          <div
+            ref={card4Ref}
+            className={`text-center cardService ${
+              card4InView ? "animate__animated animate__fadeInUp" : ""
+            }`}
+          >
             <GiCenturionHelmet color="#D72323" size="6em" />
             <p>Limpieza de obra</p>
           </div>
