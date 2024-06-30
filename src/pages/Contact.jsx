5628226418c3f1a52e3d631002ec0c48e8d3871d
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-06-30 11:53:27 -0300

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,9 +3,40 @@ import React from "react";
 function Contact() {
   return (
     <>
-      <div className="imgContact"></div>
-      <div className="d-flex justify-content-center">
-        <h2 className="titleContact">Contacto</h2>
+      <div className="hero-contact">
+        <div className="contact-content container">
+          <h2 className="titleContact">Contacto</h2>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row contact-content">
+          <div className="col-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+              dignissimos, sit eligendi possimus quo velit voluptatum iste error
+              corrupti illum amet iure adipisci architecto eaque aperiam quia
+              accusamus similique eos pariatur quod ipsam facere eum fuga aut.
+              Adipisci deserunt tempore iusto, eligendi libero facere quis ex
+              pariatur neque voluptatum labore!
+            </p>
+          </div>
+          <div className="col-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ipsa
+              voluptate ducimus nemo unde. Eligendi, aspernatur voluptas?
+              Placeat aperiam amet in quod. Saepe nesciunt commodi consequatur
+              dolor tenetur, at cupiditate autem eius tempora voluptatibus eos
+              libero in nemo nulla ratione facilis accusamus repellat quis alias
+              praesentium, ex quaerat eveniet delectus vero? Veniam, ad! Officia
+              ex minima, voluptas reprehenderit at hic beatae ea veritatis?
+              Consequuntur facilis explicabo id quae magni dignissimos eaque
+              natus cumque reprehenderit, mollitia dolorem labore quasi ullam
+              fuga tempore eum, asperiores, incidunt iste exercitationem quo
+              cum. Veniam sint ut corrupti necessitatibus, deserunt ducimus
+              nesciunt autem saepe asperiores adipisci.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
