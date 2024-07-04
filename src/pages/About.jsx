48a6e324b46510847597a9fd8864a5919a5871bb
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-07-04 09:53:25 -0300

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="hero-about">
+        <div className="about-content container">
+          <h2 className="titleAbout mt-5">Sobre nosotros</h2>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row about-content">
+          <div className="col-12">
+            <h2 className="titleQuienes">¿Quiénes Somos?</h2>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
