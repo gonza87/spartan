744f874ee3913a40cfffc2b38ea04d6bf7863b96
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-07-04 12:02:54 -0300

    stilosabout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -46,70 +46,78 @@ function About() {
               <p>
                 1. Capacitación ante el Delito y Procedimientos de Actuación
               </p>
+              <ul>
+                <li>
+                  Prevención del delito: Técnicas y tácticas para anticipar y
+                  prevenir actos delictivos.
+                </li>
+                <li>
+                  Respuesta inmediata: Protocolos y procedimientos para actuar
+                  de manera eficiente y segura ante situaciones de riesgo.
+                </li>
+                <li>
+                  Uso de tecnología: Implementación de herramientas avanzadas
+                  para la vigilancia y seguridad.
+                </li>
+              </ul>
 
-              <p className="ms-2">
-                * Prevención del delito: Técnicas y tácticas para anticipar y
-                prevenir actos delictivos.
-              </p>
-              <p className="ms-2">
-                * Respuesta inmediata: Protocolos y procedimientos para actuar
-                de manera eficiente y segura ante situaciones de riesgo.
-              </p>
-              <p className="ms-2">
-                * Uso de tecnología: Implementación de herramientas avanzadas
-                para la vigilancia y seguridad.
-              </p>
               <p>2. Atención y Recepción en Edificios y Grandes Residencias</p>
-              <p className="ms-2">
-                * Control de acceso: Técnicas para gestionar y supervisar el
-                ingreso de personas.
-              </p>
-              <p className="ms-2">
-                * Servicio al cliente: Formación en habilidades de comunicación
-                y trato con el público.
-              </p>
-              <p className="ms-2">
-                * Procedimientos de emergencia: Planificación y ejecución de
-                protocolos de evacuación y respuesta ante emergencias.
-              </p>
+              <ul>
+                <li>
+                  Control de acceso: Técnicas para gestionar y supervisar el
+                  ingreso de personas.
+                </li>
+                <li>
+                  Servicio al cliente: Formación en habilidades de comunicación
+                  y trato con el público.
+                </li>
+                <li>
+                  Procedimientos de emergencia: Planificación y ejecución de
+                  protocolos de evacuación y respuesta ante emergencias.
+                </li>
+              </ul>
             </div>
           </div>
           <div className="col-lg-6">
             <div className="contQuienes">
               <h2 className="titleQuienes">Nuestros Valores Fundamentales</h2>
-              <p>
-                * Responsabilidad: Nos comprometemos con la seguridad y el
-                bienestar de nuestros clientes.
-              </p>
-              <p>
-                * Seriedad: Tratamos cada situación con la seriedad que merece,
-                asegurando siempre la máxima protección.
-              </p>
-              <p>
-                * Atención al Cliente: La satisfacción de nuestros clientes es
-                nuestra prioridad. Ofrecemos un servicio atento y profesional en
-                todo momento.
-              </p>
+              <ul>
+                <li>
+                  Responsabilidad: Nos comprometemos con la seguridad y el
+                  bienestar de nuestros clientes.
+                </li>
+                <li>
+                  Seriedad: Tratamos cada situación con la seriedad que merece,
+                  asegurando siempre la máxima protección.
+                </li>
+                <li>
+                  Atención al Cliente: La satisfacción de nuestros clientes es
+                  nuestra prioridad. Ofrecemos un servicio atento y profesional
+                  en todo momento.
+                </li>
+              </ul>
             </div>
           </div>
           <div className="col-lg-6">
             <div className="contQuienes">
               <h2 className="titleQuienes">Opciones de Contratación</h2>
               <p>
-                En Spartan Seguridad, entendemos que cada cliente tiene
-                necesidades únicas. Por ello, ofrecemos precios flexibles y
-                varias opciones de contratación para adaptarnos a sus
-                requerimientos:
+                En <span className="titleSparta">Spartan</span> Seguridad,
+                entendemos que cada cliente tiene necesidades únicas. Por ello,
+                ofrecemos precios flexibles y varias opciones de contratación
+                para adaptarnos a sus requerimientos:
               </p>
-              <p>* Planes Básicos: Servicios esenciales de seguridad.</p>
-              <p>
-                * Planes Intermedios: Servicios de seguridad y atención al
-                cliente ampliados.
-              </p>
-              <p>
-                * Planes Premium: Servicios completos, con acceso a todas
-                nuestras capacidades y tecnologías avanzadas.
-              </p>
+              <ul>
+                <li>Planes Básicos: Servicios esenciales de seguridad.</li>
+                <li>
+                  Planes Intermedios: Servicios de seguridad y atención al
+                  cliente ampliados.
+                </li>
+                <li>
+                  Planes Premium: Servicios completos, con acceso a todas
+                  nuestras capacidades y tecnologías avanzadas.
+                </li>
+              </ul>
             </div>
           </div>
         </div>
