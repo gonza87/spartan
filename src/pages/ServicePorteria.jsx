135ec2e6323caec18eea0c5aec6fe03a3e1a9961
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-07-16 21:26:17 -0300

    porteria

--- src/pages/ServicePorteria.jsx
+++ src/pages/ServicePorteria.jsx
@@ -6,50 +6,42 @@ function ServicePorteria() {
       <div className="hero-porteria">
         <div className="porteria-content container">
           <h2 className="titlePorteria mt-5">Portería</h2>
-          <p>
+          {/* <p>
             En Spartan Seguridad, entendemos la importancia de contar con un
             servicio de portería profesional y confiable. Nuestro equipo de
             porteros está capacitado para proporcionar un entorno seguro y
             ordenado en cualquier tipo de instalación, ya sea residencial,
             comercial o industrial.
-          </p>
+          </p> */}
         </div>
       </div>
       <div className="container">
         <div className="row porteria-content">
           <div className="col-12 col-lg-6">
             <div className="contQuienes">
-              <h2 className="titleQuienes">¿Qué Ofrecemos?</h2>
               <p>
-                Control de Accesos: Supervisión y registro de entrada y salida
-                de personas y vehículos para garantizar que solo los autorizados
-                puedan acceder. Vigilancia Preventiva: Monitoreo constante de
-                las áreas comunes y perímetros para detectar y prevenir
-                cualquier actividad sospechosa. Atención al Cliente: Nuestros
-                porteros están entrenados para ofrecer un trato cordial y
-                profesional, asistiendo a visitantes y residentes con
-                información y orientación. Gestión de Correspondencia: Recepción
-                y distribución eficiente de paquetes y correspondencia,
-                asegurando que cada entrega llegue a su destinatario
-                correctamente. Respuesta Rápida: En caso de emergencias,
-                nuestros porteros están preparados para actuar de manera rápida
-                y eficaz, siguiendo los protocolos de seguridad establecidos.
+                El personal de portería se caracteriza por su excelente
+                presencia, siempre vestido con trajes elegantes que reflejan
+                profesionalismo y seriedad. Este equipo se enfoca en la tarea de
+                recepción, asegurándose de que tanto los habitantes como los
+                visitantes del lugar reciban un trato amable y respetuoso en
+                todo momento. La permanencia y la ausencia de rotación del
+                personal inspiran una confianza inquebrantable en quienes
+                frecuentan el lugar, creando un ambiente de seguridad y
+                familiaridad.
               </p>
             </div>
           </div>
           <div className="col-12 col-lg-6">
             <div className="contQuienes">
-              <h2 className="titleQuienes">¿Por Qué Elegirnos?</h2>
               <p>
-                En Spartan Seguridad, nos comprometemos a proporcionar un
-                servicio de portería que no solo cumple con los estándares más
-                altos de seguridad, sino que también añade valor a su entorno.
-                Con un enfoque en la profesionalidad y la atención al detalle,
-                nuestros porteros se convierten en una extensión de su equipo,
-                contribuyendo a un ambiente seguro y tranquilo. Confíe en
-                Spartan Seguridad para proteger lo que más importa. Contáctenos
-                hoy para obtener más información sobre cómo podemos ayudarle a
-                mantener su propiedad segura.
+                Además, el personal de portería se distingue por su perfil
+                proactivo, siempre dispuesto a solucionar problemas de manera
+                eficiente y a brindar ayuda a quien lo necesite. Su capacidad
+                para anticiparse a las necesidades de los residentes y
+                visitantes, junto con su disposición para resolver cualquier
+                inconveniente que surja, asegura una experiencia satisfactoria y
+                sin contratiempos para todos.
               </p>
             </div>
           </div>
