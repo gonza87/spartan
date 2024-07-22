5f50b4cb43ae184ab3f417efaa352b0fa93c88f8
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-07-22 11:23:22 -0300

    swetErrorForms

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -49,6 +49,21 @@ function Contact() {
         },
         (error) => {
           console.log("FAILED...", error.text);
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "center",
+            showConfirmButton: false,
+            timer: 3500,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            },
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Error inténtelo mas tarde",
+          });
         }
       );
   };
