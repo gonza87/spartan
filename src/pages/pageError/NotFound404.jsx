0b5daf6dc5a290cc3e758f572d0c76c066bf2f61
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-07-22 12:46:41 -0300

    retunHome

--- src/pages/pageError/NotFound404.jsx
+++ src/pages/pageError/NotFound404.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { GrFormNextLink } from "react-icons/gr";
 import "./NotFound404.css";
 
 function NotFound() {
@@ -11,6 +14,11 @@ function NotFound() {
               <p className="textSorry">Sorry, the page not found</p>
               <p className="textOther">The link you followed probably broken</p>
               <p className="textOther">or the page has been removed.</p>
+              <Link to={"/"}>
+                <button className="btnRetunToHome">
+                  Return to home <GrFormNextLink />
+                </button>
+              </Link>
             </div>
           </div>
         </div>
