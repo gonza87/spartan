d1d10e3cb64897203c2cea2244df3a555b219c76
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-07-26 19:47:29 -0300

    work

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -88,104 +88,102 @@ function Work() {
     <>
       <div className="hero-work">
         <div className="work-content container">
-          <h2 className="titleWork mt-5">Trabaja con nosotros</h2>
+          <h2 className="titleWork">Trabaja con nosotros</h2>
           <p className="text-center">
             Te damos la bienvenida a{" "}
             <span className="titleSparta">Spartan</span>
           </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row work-content">
-          <div className="col-12 d-flex justify-content-center">
-            <div className="contNuestraCultura">
-              <h2 className="titleCultura">Nuesta cultura</h2>
-              <p>
-                En <span className="titleSparta">Spartan</span>, creemos
-                firmemente que nuestras personas son nuestro mayor recurso y la
-                clave de nuestro éxito. Nuestro equipo está compuesto por
-                profesionales dedicados y apasionados por la seguridad, y
-                estamos siempre en la búsqueda de personas talentosas que
-                compartan nuestro compromiso con la excelencia. Nos
-                enorgullecemos de crear un entorno de trabajo donde cada miembro
-                del equipo se siente valorado y respetado. Fomentamos una
-                cultura de inclusión, colaboración y crecimiento profesional,
-                donde cada persona tiene la oportunidad de desarrollarse y
-                alcanzar su máximo potencial.
-              </p>
+          <div className="row work-content">
+            <div className="col-12 d-flex justify-content-center">
+              <div className="contNuestraCultura">
+                <h2 className="titleCultura">Nuesta cultura</h2>
+                <p>
+                  En <span className="titleSparta">Spartan</span>, creemos
+                  firmemente que nuestras personas son nuestro mayor recurso y
+                  la clave de nuestro éxito. Nuestro equipo está compuesto por
+                  profesionales dedicados y apasionados por la seguridad, y
+                  estamos siempre en la búsqueda de personas talentosas que
+                  compartan nuestro compromiso con la excelencia. Nos
+                  enorgullecemos de crear un entorno de trabajo donde cada
+                  miembro del equipo se siente valorado y respetado. Fomentamos
+                  una cultura de inclusión, colaboración y crecimiento
+                  profesional, donde cada persona tiene la oportunidad de
+                  desarrollarse y alcanzar su máximo potencial.
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div className="col-12 mt-5 mb-5">
-            <div className="col-md-12">
-              <form
-                className="contact-form"
-                ref={form}
-                onSubmit={sendEmail}
-                encType="multipart/form-data"
-              >
-                <h3 className="titleFormWork">Aplicar aquí</h3>
-                <div className="form-group">
-                  <label htmlFor="user_name">Nombre</label>
-                  <input
-                    style={{ fontSize: "1.5rem" }}
-                    className="form-control"
-                    type="text"
-                    name="user_name"
-                    id="user_name"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="user_telephone">Teléfono</label>
-                  <input
-                    style={{ fontSize: "1.5rem" }}
-                    className="form-control"
-                    type="tel"
-                    name="user_telephone"
-                    id="user_telephone"
-                    pattern="^\+?[0-9\s\-\(\)]+$"
-                    title="Por favor, ingresa un número de teléfono válido"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="user_email">Email</label>
-                  <input
-                    style={{ fontSize: "1.5rem" }}
-                    className="form-control"
-                    type="email"
-                    name="user_email"
-                    id="user_email"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="cv">Adjuntar CV</label>
-                  <input
-                    style={{ fontSize: "1.5rem" }}
-                    className="form-control"
-                    type="file"
-                    name="cv"
-                    id="cv"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="message">Mensaje</label>
-                  <textarea
-                    style={{ height: "200px", fontSize: "1.5rem" }}
-                    className="form-control"
-                    name="message"
-                    id="message"
-                    required
-                  />
-                </div>
+            <div className="col-12 mt-5 mb-5">
+              <div className="col-md-12">
+                <form
+                  className="contact-form"
+                  ref={form}
+                  onSubmit={sendEmail}
+                  encType="multipart/form-data"
+                >
+                  <h3 className="titleFormWork">Aplicar aquí</h3>
+                  <div className="form-group">
+                    <label htmlFor="user_name">Nombre</label>
+                    <input
+                      style={{ fontSize: "1.5rem" }}
+                      className="form-control"
+                      type="text"
+                      name="user_name"
+                      id="user_name"
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="user_telephone">Teléfono</label>
+                    <input
+                      style={{ fontSize: "1.5rem" }}
+                      className="form-control"
+                      type="tel"
+                      name="user_telephone"
+                      id="user_telephone"
+                      pattern="^\+?[0-9\s\-\(\)]+$"
+                      title="Por favor, ingresa un número de teléfono válido"
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="user_email">Email</label>
+                    <input
+                      style={{ fontSize: "1.5rem" }}
+                      className="form-control"
+                      type="email"
+                      name="user_email"
+                      id="user_email"
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="cv">Adjuntar CV</label>
+                    <input
+                      style={{ fontSize: "1.5rem" }}
+                      className="form-control"
+                      type="file"
+                      name="cv"
+                      id="cv"
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="message">Mensaje</label>
+                    <textarea
+                      style={{ height: "200px", fontSize: "1.5rem" }}
+                      className="form-control"
+                      name="message"
+                      id="message"
+                      required
+                    />
+                  </div>
 
-                <button type="submit" className="btnSendContact" value="Send">
-                  Enviar Mensaje
-                </button>
-              </form>
+                  <button type="submit" className="btnSendContact" value="Send">
+                    Enviar Mensaje
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
