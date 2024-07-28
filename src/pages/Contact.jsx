4c32e5156512dcf939fb71877761ca84cd76e0b6
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-07-28 17:18:38 -0300

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -101,114 +101,112 @@ function Contact() {
     <>
       <div className="hero-contact">
         <div className="contact-content container">
-          <h2 className="titleContact mt-5">Contacto</h2>
-          <p className="text-center">
+          <h2 className="titleContact">Contacto</h2>
+          <p className="text-center descriptionTitleContact">
             Contáctanos por email, whatsapp o teléfono
           </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row contact-content">
-          <div className="col-lg-4 mt-5">
-            <div
-              ref={card1Ref}
-              className={`contenedorDatos ${
-                card1InView ? "animate__animated animate__fadeInLeftBig" : ""
-              }`}
-            >
-              <div>
-                <p className="titleDatos">Teléfono:</p>
-                <p className="textoDatos">2683 8533</p>
-                <IoCallOutline
-                  size={20}
-                  className="btnDatos"
-                  onClick={handleCall}
-                />
-                <FaWhatsapp
-                  size={20}
-                  className="ms-3 btnDatos"
-                  onClick={handleWhatsappClick}
-                />
-              </div>
+          <div className="row contact-content">
+            <div className="col-lg-4 mt-5">
+              <div
+                ref={card1Ref}
+                className={`contenedorDatos ${
+                  card1InView ? "animate__animated animate__fadeInLeftBig" : ""
+                }`}
+              >
+                <div>
+                  <p className="titleDatos">Teléfono:</p>
+                  <p className="textoDatos">2683 8533</p>
+                  <IoCallOutline
+                    size={20}
+                    className="btnDatos"
+                    onClick={handleCall}
+                  />
+                  <FaWhatsapp
+                    size={20}
+                    className="ms-3 btnDatos"
+                    onClick={handleWhatsappClick}
+                  />
+                </div>
 
-              <div className="mt-4">
-                <p className="titleDatos">Email:</p>
-                <p className="textoDatos">info@spartanseguridad.com.uy</p>
-                <VscSend
-                  className="btnDatos"
-                  size={20}
-                  onClick={handleSendEmail}
-                />
-                <IoCopyOutline
-                  className="ms-3 btnDatos"
-                  size={20}
-                  onClick={handleCopyEmail}
-                />
-              </div>
+                <div className="mt-4">
+                  <p className="titleDatos">Email:</p>
+                  <p className="textoDatos">info@spartanseguridad.com.uy</p>
+                  <VscSend
+                    className="btnDatos"
+                    size={20}
+                    onClick={handleSendEmail}
+                  />
+                  <IoCopyOutline
+                    className="ms-3 btnDatos"
+                    size={20}
+                    onClick={handleCopyEmail}
+                  />
+                </div>
 
-              <div className="mt-4">
-                <p className="titleDatos">Horario de atención al cliente:</p>
-                <p className="textoDatos"> Lunes a Viernes - 9 a 18 h</p>
+                <div className="mt-4">
+                  <p className="titleDatos">Horario de atención al cliente:</p>
+                  <p className="textoDatos"> Lunes a Viernes - 9 a 18 h</p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-lg-8 mb-5">
-            <form className="contact-form" ref={form} onSubmit={sendEmail}>
-              <h2 className="titleFormContact">
-                Si deseas más información, puedes ponerte en contacto con
-                nosotros a través del siguiente formulario.
-              </h2>
-              <div className="form-group">
-                <label htmlFor="user_name">Nombre</label>
-                <input
-                  style={{ fontSize: "1.5rem" }}
-                  className="form-control"
-                  type="text"
-                  name="user_name"
-                  id="user_name"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="user_telephone">Teléfono</label>
-                <input
-                  style={{ fontSize: "1.5rem" }}
-                  className="form-control"
-                  type="tel"
-                  name="user_telephone"
-                  id="user_telephone"
-                  pattern="^\+?[0-9\s\-\(\)]+$"
-                  title="Por favor, ingresa un número de teléfono válido"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="user_email">Email</label>
-                <input
-                  style={{ fontSize: "1.5rem" }}
-                  className="form-control"
-                  type="email"
-                  name="user_email"
-                  id="user_email"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="message">Mensaje</label>
-                <textarea
-                  style={{ height: "200px", fontSize: "1.5rem" }}
-                  className="form-control"
-                  name="message"
-                  id="message"
-                  required
-                />
-              </div>
+            <div className="col-lg-8 mb-5">
+              <form className="contact-form" ref={form} onSubmit={sendEmail}>
+                <h2 className="titleFormContact">
+                  Si deseas más información, puedes ponerte en contacto con
+                  nosotros a través del siguiente formulario.
+                </h2>
+                <div className="form-group">
+                  <label htmlFor="user_name">Nombre</label>
+                  <input
+                    style={{ fontSize: "1.5rem" }}
+                    className="form-control"
+                    type="text"
+                    name="user_name"
+                    id="user_name"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="user_telephone">Teléfono</label>
+                  <input
+                    style={{ fontSize: "1.5rem" }}
+                    className="form-control"
+                    type="tel"
+                    name="user_telephone"
+                    id="user_telephone"
+                    pattern="^\+?[0-9\s\-\(\)]+$"
+                    title="Por favor, ingresa un número de teléfono válido"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="user_email">Email</label>
+                  <input
+                    style={{ fontSize: "1.5rem" }}
+                    className="form-control"
+                    type="email"
+                    name="user_email"
+                    id="user_email"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="message">Mensaje</label>
+                  <textarea
+                    style={{ height: "200px", fontSize: "1.5rem" }}
+                    className="form-control"
+                    name="message"
+                    id="message"
+                    required
+                  />
+                </div>
 
-              <button type="submit" className="btnSendContact" value="Send">
-                Enviar Mensaje
-              </button>
-            </form>
+                <button type="submit" className="btnSendContact" value="Send">
+                  Enviar Mensaje
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
